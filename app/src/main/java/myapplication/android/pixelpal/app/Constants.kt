9a72ae5daccf1135a8ce5ff420f3eae75d9be710
Shadow Berserker <iuliaglad07@gmail.com>
2024-11-13 21:34:35 +0300

    Added data and domain layers

--- app/src/main/java/myapplication/android/pixelpal/app/Constants.kt
+++ app/src/main/java/myapplication/android/pixelpal/app/Constants.kt
@@ -2,7 +2,7 @@ package myapplication.android.pixelpal.app
 
 class Constants {
     companion object{
-        const val API_KEY = ""
-        const val BASE_URL = ""
+        const val API_KEY = "e2b544813c594d0a8e3d65dca3fd132f"
+        const val BASE_URL = "https://api.rawg.io/api/"
     }
 }
